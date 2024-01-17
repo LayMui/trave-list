6135469e181d67deacbaf07585f8c2362996a3c1
LayMui <mui_ume@hotmail.com>
2024-01-17 17:00:40 +0800

    Add render list

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Chargers", quantity: 1, packed: true },
+];
+
+
 export default function App()
 {
-  return <div className="app">
+  return ( <div className="app">
     <Logo />
     <Form/>
     <PackingList/>
+    <Stats/>
   </div>
+  );
 }
 
 function Logo() {
   return <h1>🐐Far Away🎋</h1>
 }
+
 function Form() {
   return (
     <>
@@ -18,10 +29,25 @@ function Form() {
     </>
   );
 }
+
 function PackingList() {
-  return <div className="list">
-    LIST
+  return
+  <div className="list"> 
+    <ul className="list">
+      {initialItems.map(item => <Item item={item}/>)}
+    </ul>
   </div>
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span> 
+        {item.quantity} {item.description}
+        </span>
+      <button>X&times;</button>
+    </li>
+  )
 }
 function Stats() {
   return (
