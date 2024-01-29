14021bd64ff1632f737f8d2fafd2fde4ef3c6ce7
LayMui <mui_ume@hotmail.com>
2024-01-29 16:25:22 +0800

    handleAddItem issue

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
+import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Chargers", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Chargers", quantity: 1, packed: true },
+// ];
 
 
 export default function App()
 {
+  const [items, setItems] = useState([])
+
+  function handleAddItems(item)
+  {
+    setItems((item) => [...items, item])
+  }
+
   return ( <div className="app">
     <Logo />
-    <Form/>
-    <PackingList/>
-    <Stats/>
+    <Form onAddItems={handleAddItems}/>
+    <PackingList items={items}/>
+    <Stats items={items}/>
   </div>
   );
 }
@@ -21,28 +29,62 @@ function Logo() {
   return <h1>🐐Far Away🎋</h1>
 }
 
-function Form() {
+function Form({onAddItems}) {
+
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
+ 
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if(!description) return;
+    const newItem = {description, quantity, packed:false, id: Date.now()};
+
+      console.log(newItem)
+
+      onAddItems(newItem)
+
+      setDescription("");
+      setQuantity(1)
+  }
+
   return (
-    <>
-      <div className="add-form"></div>
+      <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </>
+      <select
+      value={quantity}
+      onChange={(e) => setQuantity(Number(e.target.value))}>
+        {Array.from({length: 20}, (_, i) => i + 1).map((num) => 
+        <option value={num} key={num}>
+          {num}
+          </option>)}
+      
+      </select>
+      <input 
+        type="text" 
+        placeholder="Item..." 
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
-function PackingList() {
-  return
+function PackingList({items}) {
+  return (
   <div className="list"> 
     <ul className="list">
-      {initialItems.map(item => <Item item={item}/>)}
+      {items.map(item => <Item item={item} key={item.id}/>)}
     </ul>
   </div>
+  )
 }
 
 function Item({item}) {
   return (
     <li>
-      <span> 
+      <span style={item.packed ? {textDecoration: 'line-through'}: {}}> 
         {item.quantity} {item.description}
         </span>
       <button>X&times;</button>
