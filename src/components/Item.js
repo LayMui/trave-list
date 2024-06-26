af32f38123f898cf606384e708dc8eb255061de5
LayMui <mui_ume@hotmail.com>
2024-06-26 17:38:55 +0800

    Add homepage

--- src/components/Item.js
+++ src/components/Item.js
@@ -7,7 +7,7 @@ export default function Item({ item, onDeleteItem, onToggleItem }) {
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+      <button data-testid="close" onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
--- src/components/PackingList.js
+++ src/components/PackingList.js
@@ -37,8 +37,8 @@ export default function PackingList({
       </ul>
 
       <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
+        <select id="mySelect" value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option data-testid="input" value="input">Sort by input order</option>
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
